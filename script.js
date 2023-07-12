97376ecee121a420f131bb59ee0756339a7499cb
kamranZap <123232712+kamranZap@users.noreply.github.com>
2023-07-12 20:16:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+document.querySelectorAll("input")[0].addEventListener("click", updateButton1);
+function updateButton1(){
+  document.querySelectorAll("input")[0].style.backgroundColor = "#ADB3BC";
+  document.querySelectorAll("input")[0].style.color = "white";
+document.getElementById('myform').action = "submit.html";
+}
+document.querySelectorAll("input")[1].addEventListener("click", updateButton2);
+function updateButton2(){
+  document.querySelectorAll("input")[1].style.backgroundColor = "#ADB3BC";
+  document.querySelectorAll("input")[1].style.color = "white";
+document.getElementById('myform').action = "submit1.html";
+}
+document.querySelectorAll("input")[2].addEventListener("click", updateButton3);
+function updateButton3(){
+  document.querySelectorAll("input")[2].style.backgroundColor = "#ADB3BC";
+  document.querySelectorAll("input")[2].style.color = "white";
+document.getElementById('myform').action = "submit2.html";
+}
+document.querySelectorAll("input")[3].addEventListener("click", updateButton4);
+function updateButton4(){
+  document.querySelectorAll("input")[3].style.backgroundColor = "#ADB3BC";
+  document.querySelectorAll("input")[3].style.color = "white";
+document.getElementById('myform').action = "submit3.html";
+}
+document.querySelectorAll("input")[4].addEventListener("click", updateButton5);
+function updateButton5(){
+  document.querySelectorAll("input")[4].style.backgroundColor = "#ADB3BC";
+  document.querySelectorAll("input")[4].style.color = "white";
+document.getElementById('myform').action = "submit4.html";
+}
